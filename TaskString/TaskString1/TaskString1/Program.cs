d4bea3d13f4d2c325a2b6fd13285abe7fc2cc42d
Yura <strelc.yura@mail.ru>
2021-04-10 15:12:01 +0300

    add taskString1

---
+++ TaskString/TaskString1/TaskString1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TaskString1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            for (int i=0; i<= a.Length-1; i++)
+            {
+                Console.WriteLine($"-{a[i]}-");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
