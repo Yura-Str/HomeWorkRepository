0ac262dee85900d0eb6d1b90b5398a5fcd4e70b7
Yura <strelc.yura@mail.ru>
2021-04-10 13:59:34 +0300

    fix Task5

--- TaskArray/TaskArray5/Program.cs
+++ TaskArray/TaskArray5/Program.cs
@@ -6,7 +6,30 @@ namespace TaskArray5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Input number of values in an array");
+            var n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int a = 0; a < arr.Length; a++)
+            {
+                arr[a] = Convert.ToInt32(Console.ReadLine());
+            }
+            int consilience = 0;
+            for (int a = 0; a < arr.Length; a++)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (a == i)
+                    {
+
+                    }
+                    else if (arr[a] == arr[i])
+                    {
+                        consilience++;
+                    }
+                }
+                Console.WriteLine($"{arr[a]}:{consilience}");
+                consilience = 0;
+            }
         }
     }
 }
